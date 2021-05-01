68683359a8f7e77effe10dc73c1d4796be3f98ca
Tien Quang <t.quang45@gmail.com>
2021-05-01 18:43:55 -0500

    Sudoku class that reads a puzzle.txt file to get the Sudoku puzzle it will solve

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Sudoku puzzle = new Sudoku();
+        if(!puzzle.loadSudoku()){return;}
+        puzzle.printSudoku();
     }
 }
