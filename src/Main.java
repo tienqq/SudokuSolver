87dd009aec44877b7bbf1fe05ea0efc36c58381e
Tien Quang <t.quang45@gmail.com>
2021-05-05 16:17:30 -0500

    implemented a backtracking algo to solve sudoku

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,26 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Sudoku puzzle = new Sudoku();
-        if(!puzzle.loadSudoku()){return;}
-        puzzle.printSudoku();
+        int[][]puzzle = {{0,0,7,0,1,8,0,0,9},
+                         {0,0,8,5,0,0,7,0,2},
+                         {0,0,6,0,7,0,0,4,0},
+                         {0,7,5,0,0,6,0,0,0},
+                         {2,8,0,9,0,4,0,3,7},
+                         {0,0,0,1,0,0,5,9,0},
+                         {0,1,0,0,3,0,4,0,0},
+                         {4,0,3,0,0,5,2,0,0},
+                         {7,0,0,6,4,0,9,0,0}};
+        SudokuSolver solveBacktrack = new SudokuSolver(puzzle);
+        System.out.println("Sudoku puzzle that we're gonna solve using backtracking:");
+        solveBacktrack.printSolvedPuzzle();
+        long start = System.currentTimeMillis();
+        if(solveBacktrack.backtrack(solveBacktrack.puzzle)){
+            System.out.println("\nSolution: \n");
+            solveBacktrack.printSolvedPuzzle();
+            System.out.println("\nSolved in "+(System.currentTimeMillis()-start)+"ms");
+        }
+        else{
+            System.out.println("No solution found");
+        }
     }
 }
---
+++ src/SudokuSolver.java
@@ -0,0 +1,67 @@
+import java.util.Arrays;
+
+public class SudokuSolver {
+    public int[][]puzzle;
+    private final int size = 9;
+
+    public SudokuSolver(int[][]puzzle){
+        this.puzzle = puzzle;
+    }
+
+    public boolean backtrack(int[][] puzzle){
+        return solveBacktrack(puzzle, 0 , 0);
+    }
+    private boolean solveBacktrack(int[][] puzzle, int row, int col){
+        //if we reached the 8th row and 9th column, we are done
+        if(row == size-1 && col==size){
+            return true;
+        }
+        //if col = 9 then move onto next row and reset col back to 0
+        if(col == size){
+            row++;
+            col=0;
+        }
+        //check to see if there isn't already a value in the current position
+        if(puzzle[row][col] != 0){
+            return solveBacktrack(puzzle, row, col+1);
+        }
+
+        //iterate through possible values
+        for(int i=1; i<10; i++){
+            //check to see if it's safe to place value at the position
+            if(isSafe(puzzle,row,col,i)){
+                puzzle[row][col]=i;
+
+                //check for next column possibility
+                if(solveBacktrack(puzzle,row,col+1)) return true;
+            }
+            //reset and move onto next value
+            puzzle[row][col]=0;
+        }
+        return false;
+    }
+    private boolean isSafe(int[][]puzzle, int row, int col, int num){
+        //check to see if the num is already in the row
+        for(int i=0;i<size;i++){
+            if(puzzle[row][i]==num) return false;
+        }
+        //check to see if num is already in the column
+        for(int i=0;i<size;i++){
+            if(puzzle[i][col]==num) return false;
+        }
+        //check to see if num is in the 3x3 grid
+        int rowIndex = row - row % 3;
+        int colIndex = col - col % 3;
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if(puzzle[i+rowIndex][j+colIndex]==num) return false;
+            }
+        }
+        return true;
+    }
+
+    public void printSolvedPuzzle(){
+        for(int[] x: puzzle)
+            System.out.println(Arrays.toString(x));
+    }
+}
